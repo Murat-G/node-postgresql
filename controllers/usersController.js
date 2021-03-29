c819008d1f051617bfa104fa87c65f75cb304080
MuratG <muratgozderesi34@gmail.com>
2021-03-30 02:26:30 +0300

    add_user function

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,3 +1,31 @@
-exports.get_users = function(req,res,next) {
-    res.render('users');
+const UserModel = require("../models/User");
+
+exports.get_users = async (req, res, next) => {
+  //get users from db
+  try {
+    const userList = await UserModel.findAll({});
+    res.render("users", { userList });
+  } catch (error) {
+    res.send("An error occured");
+  }
+};
+
+
+exports.show_add_user_form = (req, res) => {
+  res.render("addUser");
+};
+
+
+//on post request
+exports.add_user = async (req, res) => {
+  // add to db
+  try {
+    const newUser = await UserModel.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+    });
+    res.redirect("/users");
+  } catch (error) {
+    res.send("An error occurred.");
+  }
 };
--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,7 @@ const usersController = require('../controllers/usersController')
 
 /* GET users listing. */
 router.get('/', usersController.get_users);
+router.get("/add", usersController.show_add_user_form);
+router.post("/add",usersController.add_user);
 
 module.exports = router;
