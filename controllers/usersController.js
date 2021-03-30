ee31661600a9897f26f2925fdb5e0a4a15d7680b
MuratG <muratgozderesi34@gmail.com>
2021-03-30 15:20:06 +0300

    update_user function

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -12,7 +12,7 @@ exports.get_users = async (req, res, next) => {
 
 
 exports.show_add_user_form = (req, res) => {
-  res.render("addUser");
+  res.render("addUser",{user: undefined});
 };
 
 
@@ -40,5 +40,30 @@ exports.delete_user = async (req, res) => {
     res.redirect("/users");
   }catch (error) {
     console.log("error", error);
+  }
+}
+
+exports.show_edit_user_page = async (req, res) => {
+  try {
+    const user = await UserModel.findOne({ where: { id: req.params.id } });
+    res.render("addUser", { user });
+  } catch (error) {
+    res.send("An error occured");
+  }
+};
+
+exports.update_user = async (req,res) => {
+  let updatedObject = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+  };
+  try {
+   let result = await UserModel.update(updatedObject,{
+      returning: true,
+      where: { id: req.params.id },
+    });
+    res.redirect("/users");
+  } catch (error) {
+    res.send("An error occured");
   }
 }
--- routes/users.js
+++ routes/users.js
@@ -4,8 +4,13 @@ const usersController = require('../controllers/usersController')
 
 /* GET users listing. */
 router.get('/', usersController.get_users);
+
 router.get("/add", usersController.show_add_user_form);
 router.post("/add",usersController.add_user);
+
 router.get("/:id/delete", usersController.delete_user);
+
+router.get("/:id/edit", usersController.show_edit_user_page);
+router.post("/:id/edit", usersController.update_user);
 
 module.exports = router;
